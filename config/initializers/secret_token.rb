a30e9409c71b68c33134a84a3de559493de34dd5
rishabh <rishabh@rishabh-HP.(none)>
2014-09-20 16:56:51 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voucherdonor::Application.config.secret_token = '964804c7ffe0d177a13f0228c05b1af80a9ba617f6c9c7dd408c4ab7e0ae310561b006e6e303face2a693bef27b1e0b29fa9e61e2786b4610115bcb49183432f'
