476e57907bd0d881533445a35d617c3a7a5b773c
rishabh <rishabh@rishabh-HP.(none)>
2014-09-20 17:28:27 +0530

    homepage

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,29 @@
+function ele(id) {
+    return document.getElementById(id);
+}
+
+function ele_show(id) {
+    document.getElementById(id).style.display="";
+}
+
+function ele_hide(id) {
+    document.getElementById(id).style.display="none";
+}
+
+function ele_toggle(id) {
+    if(document.getElementById(id).style.display=="none") {
+	document.getElementById(id).style.display="";
+    } else {
+	document.getElementById(id).style.display="none";
+    }
+}
+
+function submit_ajax_form(id) {
+    $("#"+id).trigger("submit.rails");
+}
+
+function get_hash_keys(hsh) {
+    var keys = [];
+    for(var i in hsh) { if (hsh.hasOwnProperty(i)) keys.push(i); }
+    return keys;
+}
