de2aef3698bffb7a5da06a10f5355f4eb661c65a
Bijendra <bijendra.biju@gmail.com>
2014-09-21 06:26:37 +0530

    cron jobs

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,12 +18,12 @@ class User
   field :reset_password_sent_at, :type => Time
 
   ## Rememberable
-  field :remember_created_at, :type => Time
+  field :remember_created_at, :type => DateTime
 
   ## Trackable
   field :sign_in_count,      :type => Integer, :default => 0
-  field :current_sign_in_at, :type => Time
-  field :last_sign_in_at,    :type => Time
+  field :current_sign_in_at, :type => DateTime
+  field :last_sign_in_at,    :type => DateTime
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
   field :first_name, :type => String, :default => ""
@@ -39,7 +39,7 @@ class User
   has_many :coupons
   ## Confirmable
   # field :confirmation_token,   :type => String
-  field :confirmed_at,         :type => Time
+  field :confirmed_at,         :type => DateTime
   # field :confirmation_sent_at, :type => Time
   # field :unconfirmed_email,    :type => String # Only if using reconfirmable
 
