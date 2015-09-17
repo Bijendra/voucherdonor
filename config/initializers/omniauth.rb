59c39ea2abfb7a470715cbf139601b08f9dca48b
Bijendra <bijendra.biju@gmail.com>
2015-09-17 18:06:34 +0530

    Invalid scope for fb permission and precompiled asset not loading in production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do    
   if(Rails.env.development?)
-    provider :facebook, '179716845394813', 'bdfca7d1c57344d6deec30f95e70d8f0', {:scope => "email, user_location, user_interests, user_likes, user_friends,manage_friendlists "}      
+    provider :facebook, '179716845394813', 'bdfca7d1c57344d6deec30f95e70d8f0', {:scope => "email, user_location, user_interests, user_likes, user_friends"}      
   else
-    provider :facebook, '406135692869624', '1053ff9e1ebcfc1f10da2a69e606b20c', {:scope => "email, user_location, user_interests, user_likes, user_friends,manage_friendlists "}      
+    provider :facebook, '406135692869624', '1053ff9e1ebcfc1f10da2a69e606b20c', {:scope => "email, user_location, user_interests, user_likes, user_friends"}      
   end
 end
